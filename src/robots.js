c89f98f474a1b89fd77cb0ae354cfb77b3d239ea
Mary Felsheim-Lee <lee.marytherese17@gmail.com>
2022-06-30 12:28:29 -0400

    Complete robofriends app

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Allaan Blake",
+    username: "ABtheGreat",
+    email: "allaanthegreat@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Spice Boy",
+    username: "spicy",
+    email: "spiceboy@gmail.com",
+  },
+  {
+    id: 3,
+    name: "J. J. the Robot",
+    username: "jjrobo",
+    email: "jjtherobot@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Bob the Snowblower",
+    username: "bobthesnowblower",
+    email: "bobthesnowblower@gmail.com",
+  },
+];
